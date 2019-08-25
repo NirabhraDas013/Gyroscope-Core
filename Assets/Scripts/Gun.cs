10deb2b85edcb18ab316a5a974b5d31c8d2b85f1
NirabhraDas013 <nirabhra12709@gmail.com>
2019-08-25 12:23:06 +0530

    Gyroscope Core Files

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+	public float damage = 10f;
+	public float range = 100f;
+
+	public Camera fpsCam;
+	public ParticleSystem muzzleFlash;
+	public GameObject impactEffect;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void Shoot()
+	{
+		muzzleFlash.Play ();
+
+		RaycastHit hit;
+		if(Physics.Raycast (fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+		{
+			Debug.Log (hit.transform.name);
+		}
+
+		GameObject impactGO = Instantiate (impactEffect, hit.point, Quaternion.LookRotation (hit.normal));
+		Destroy (impactGO, 1.0f);
+	}
+}
---
+++ Assets/Scripts/GyroController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroController : MonoBehaviour
+{
+	private bool gyroEnabled;
+	private Gyroscope gyro;
+
+	private GameObject cameraContainer;
+	private Quaternion rot;
+
+	private Quaternion origin = Quaternion.identity;
+
+	private void Start()
+	{
+		cameraContainer = new GameObject ("CameraContainer");
+		cameraContainer.transform.position = transform.position;
+
+		transform.SetParent (cameraContainer.transform);
+		gyroEnabled = EnableGyro ();
+	}
+
+	private void Update()
+	{
+		if(gyroEnabled)
+		{
+			transform.localRotation = gyro.attitude * rot;
+		}
+	}
+
+	private bool EnableGyro()
+	{
+		if(SystemInfo.supportsGyroscope)
+		{
+			gyro = Input.gyro;
+			gyro.enabled = true;
+
+			cameraContainer.transform.rotation = Quaternion.Euler (90f, 90f, 0);
+			rot = new Quaternion (0, 0, 1, 0);
+
+			return true;
+		}
+
+		return false;
+	}
+
+	void OnGUI() {
+		GUILayout.Label(origin.eulerAngles+" <- origin");
+		GUILayout.Label(Input.gyro.attitude.eulerAngles+" <- gyro");
+		GUILayout.Label(Quaternion.Inverse(Input.gyro.attitude).eulerAngles+" <- inv gyro");
+		GUILayout.Label(transform.localRotation.eulerAngles+" <- localRotation");
+	}
+}
